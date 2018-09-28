7d48b45d29eb3595e5944d6fe7fb078838965d6e
yangalan123 <1049909407@qq.com>
2018-09-28 23:43:09 +0800

    README updated for users' convenience

--- SPWCF.sh
+++ SPWCF.sh
@@ -1,2 +1,2 @@
-python SPWCF_prediction.py train_hownet sememe_all hownet.txt_test_input output_SPWCF_test model_SPWCF_test
-python scorer.py output_SPWCF_test hownet.txt_test_answer
+python2 SPWCF_prediction.py train_hownet sememe_all hownet.txt_test_input output_SPWCF_test model_SPWCF_test
+python2 scorer.py output_SPWCF_test hownet.txt_test_answer
--- data_generator.sh
+++ data_generator.sh
@@ -1,3 +1,3 @@
-python hownet_corpus_data_picker.py hownet.txt embedding_200.txt train_data
-python test_data_generator.py hownet.txt train_data
-python Sememe_PMI_Matrix_Generator.py train_hownet PMI.txt
+python2 hownet_corpus_data_picker.py hownet.txt embedding_200.txt train_data
+python2 test_data_generator.py hownet.txt train_data
+python2 Sememe_PMI_Matrix_Generator.py train_hownet PMI.txt
